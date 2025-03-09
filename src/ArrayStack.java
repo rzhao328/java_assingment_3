58f4418ad4d2b2b0fa619cecf8f0b5fbef013205
Jian Gong <gongjian1981@gmail.com>
2025-03-09 17:49:54 -0400

    fix: fix code in ArrayStack

--- src/ArrayStack.java
+++ src/ArrayStack.java
@@ -4,41 +4,59 @@ public class ArrayStack<T> implements StackADT<T> {
     private T[] array; // this array holds the items in the stack
     private int top;
 
+    /**
+     * default constructor
+     */
     public ArrayStack() {
         this(10);
-        top = -1;
     }
 
+    /**
+     * constructor with a initial of the capacity of this stack
+     * @param initCapacity the capacity of this stack
+     */
     public ArrayStack(int initCapacity) {
         array = (T[]) new Object[initCapacity];
         top = -1;
     }
 
+    /**
+     * push the element onto the stack
+     *
+     * @param element data item to be pushed onto stack
+     */
     public void push(T element) {
-
+        if (top == array.length - 1) {
+            this.expandCapacity();
+        }
+        array[++top] = element;
     }
 
+    /**
+     * pop the first element of the stack
+     *
+     * @return the first element of the stack
+     * @throws CollectionException when the stack is empty
+     */
     public T pop() throws CollectionException {
-        if (top == array.length - 1) throw new CollectionException("Stack is empty");
-
-        T item = array[top + 1];
-        array[top + 1] = null;
-        top++;
-
+        if (this.isEmpty()) {
+            throw new CollectionException("Stack is empty");
+        }
+        T item = array[top];
+        array[top--] = null;
         return item;
     }
 
     public T peek() throws CollectionException {
-        if (top == array.length - 1) {
+        if (this.isEmpty()) {
             throw new CollectionException("Stack is empty");
         }
-            // check is the stack is empty, throw an exception
+        // check is the stack is empty, throw an exception
         return array[top];//return the top element without removing it
     }
 
     public boolean isEmpty() {
-        if (top == -1) return true;
-        else return false;
+        return top == -1;
     }
 
     public int size() {
@@ -54,7 +72,9 @@ public class ArrayStack<T> implements StackADT<T> {
     }
 
     public String toString() {
-        if (top == -1) return "Empty Stack";
+        if (top == -1) {
+            return "Empty Stack";
+        }
 
         StringBuilder result = new StringBuilder();
         for (int i = top + 1; i < array.length; i++){
@@ -63,7 +83,7 @@ public class ArrayStack<T> implements StackADT<T> {
                 result.append(", ");
             }
         }
-            return array[top].toString();
+        return result.toString();
     }
 
     private void expandCapacity() {
