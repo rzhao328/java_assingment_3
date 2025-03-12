4d4279245399567f044e70b93cbabd2aa668ed6c
Jian Gong <gongjian1981@gmail.com>
2025-03-12 18:40:45 -0400

    feat: complete function in CampusWalk

--- src/CampusWalk.java
+++ src/CampusWalk.java
@@ -1,29 +1,39 @@
 import java.util.Stack;
 
+/**
+ *
+ */
 public class CampusWalk {
-    private Map map;
-
+    private Map map; //
+    private static final int HEXAGON_SIZE = 6; //
+    /**
+     * constructor
+     * @param filename the location of file that will initiate the MapFrame
+     * @param showMap whether to show the GUI of the map
+     */
     public CampusWalk(String filename, boolean showMap) {
         try {
             map = new Map(filename);
-        } catch (Exception e) {
-            System.out.println("Error occurred");
-            return;
-        }
-        if (map != null) {
             if (showMap) {
                 map.showGUI();
             } else {
                 map.hideGUI();
             }
+        } catch (Exception e) {
+            System.out.println("Error occurred");
         }
     }
 
+    /**
+     * Count and return the number of goose cells surrounding the given cell
+     * @param cell the cell to be count
+     * @return the number of goose cells surrounding the given cell
+     */
     public int neighbourGooseCount(Hexagon cell) {
         int count = 0;
-        for (int i = 0; i < 6; i++) {
-            Hexagon[] neighbours = new Hexagon[]{cell.getNeighbour(i)};//我不确定这一行是对的！！！！！
-            Hexagon neighbour = neighbours[i];
+        // get all neighbour cells
+        for (int i = 0; i < HEXAGON_SIZE; i++) {
+            Hexagon neighbour = cell.getNeighbour(i);
             if (neighbour != null && neighbour.isGooseCell()) {
                 count++;
             }
@@ -31,79 +41,110 @@ public class CampusWalk {
         return count;
     }
 
+    /**
+     * Determine the next cell to walk onto from the current cell
+     *
+     * @param cell current cell
+     * @return the next cell to walk onto from the current cell
+     */
     public Hexagon findBest(Hexagon cell) {
         Hexagon bestChoice = null;
-        int minGooseCount = Integer.MAX_VALUE;
-
-        // firstly, find if curr cell adjacent to the end cell, go to the end cell
-        for (int j = 0; j < 6; j++) {
-            Hexagon[] neighbours = new Hexagon[]{cell.getNeighbour(j)};//我不确定这一行是对的！！！！！
-            if (neighbours[j].isEnd()) {
-                return neighbours[j];
+        // loop each neighbour cell of current cell
+        for (int i = 0; i < HEXAGON_SIZE; i++) {
+            Hexagon neighbour = cell.getNeighbour(i);
+            if (neighbour == null) {
+                continue; // this cell doesn't exist, skip
             }
-        }
-        // if curr cell isn't adjacent to the end cell, do other basic rules
-        for (int i = 0; i < 6; i++) {
-            Hexagon[] neighbours = new Hexagon[]{cell.getNeighbour(i)};//我不确定这一行是对的！！！！！
-            int neighbourGooseCount = neighbourGooseCount(cell.getNeighbour(i));
-            // if the next cell is a goose cell or null or marked or more than 2 goose neighbour, go to next cell
-            if (neighbours[i] == null && neighbours[i].isGooseCell() && neighbourGooseCount > 2 && neighbours[i].isMarked()) {
+            // if it's the end cell, return directly
+            if (neighbour.isEnd()) {
+                return neighbour;
+            }
+            // if the next cell is a goose cell, skip;
+            if (neighbour.isGooseCell()) {
                 continue;
             }
-            //if curr is adjacent to one or more cells that contain a book, go to the book cell neighbour with the smallest index
-            if (neighbours[i].isBookCell()) {
-                bestChoice = neighbours[i];
+            // the next cell does not have a goose-neighbour-count (see below) of 3 or more, skip
+            if (neighbourGooseCount(neighbour) > 2) {
+                continue;
             }
-
-            //if curr is adjacent to one or more grass cells, go to the grass cell neighbour with the smallest index
-            // that has the lowest goose neighbour
-            else if (neighbours[i].isGrassCell()) {
-                for (int k = 0; k < 6 - i; k++) {
-                    if (neighbourGooseCount(cell.getNeighbour(i + k)) < minGooseCount) {
-                        bestChoice = neighbours[i + k];
+            // if the next cell is marked， skip
+            if (neighbour.isMarked()) {
+                continue;
+            }
+            // if curr is adjacent to one or more cells that contain a book, go to the book cell neighbour with the smallest index (
+            if (neighbour.isBookCell()) {
+                bestChoice = neighbour;
+                break;
+            }
+        }
+        if (bestChoice == null) { // bestChoice is null here means no book cell or end cell is adjacent
+            /* if curr is adjacent to one or more grass cells,
+             * go to the grass cell neighbour with the smallest index
+             * that has the lowest goose-neighbour-count.
+             */
+            int lowestGooseCount = Integer.MAX_VALUE;
+            for (int i = 0; i < HEXAGON_SIZE; i++) {
+                Hexagon neighbour = cell.getNeighbour(i);
+                if (neighbour != null && neighbour.isGrassCell()) {
+                    int gooseCount = neighbourGooseCount(neighbour);
+                    if (gooseCount < lowestGooseCount) {
+                        lowestGooseCount = gooseCount;
+                        bestChoice = neighbour;
                     }
                 }
             }
-
-            //if curr is adjacent to one or more snow cells, go to the snow cell with the smallest index
-            else if (neighbours[i].isSnowCell()) {
-                bestChoice = neighbours[i];
-
-            }
-
-            //if none of these conditions are met, return null to indicate that you cannot proceed and must backtrack
-            else {
-                bestChoice = null;
+        }
+        if (bestChoice == null) { // bestChoice is null here means no grass cell with 2 or less goose-neighbour-count cell is adjacent
+            /* if curr is adjacent to one or more snow cells,
+             * go to the snow cell with the smallest index.
+             */
+            for (int i = 0; i < HEXAGON_SIZE; i++) {
+                Hexagon neighbour = cell.getNeighbour(i);
+                if (neighbour != null && neighbour.isSnowCell()) {
+                    bestChoice = neighbour;
+                }
             }
         }
         return bestChoice;
     }
 
+    /**
+     * Determine the path from the starting point to the end cell
+     *
+     * @return
+     */
     public String findPath() {
-        if (map == null) return "No path found";
+        if (this.map == null) {
+            return "No path found";
+        }
+        // initialize Stack
         Stack<Hexagon> stack = new Stack<>();
-        Hexagon start = map.getStart();
-        if (start == null) return "No path found";
+        Hexagon start = this.map.getStart();
+        if (start == null) {
+            return "No path found";
+        }
+        // push the starting cell onto S
         stack.push(start);
+        // set a boolean variable running to be true
         boolean running = true;
-        StringBuilder pathString = new StringBuilder();
+        // mark the starting cell as in-stack
         start.markInStack();
+        StringBuilder pathString = new StringBuilder();
         Hexagon current = null;
         while (running && !stack.isEmpty()) {
             current = stack.peek();
             pathString.append(current.getID()).append(" ");
             if (current.isEnd()) {
                 running = false;
-                break;
             }
-        }
-        Hexagon next = findBest(current);
-        if (next == null) {
-            stack.pop();
-            current.isMarkedOutStack();
-        } else {
-            stack.push(next);
-            next.markInStack();
+            Hexagon next = findBest(current);
+            if (next == null) {
+                stack.pop();
+                current.markOutStack();
+            } else {
+                stack.push(next);
+                next.markInStack();
+            }
         }
         if (!running) {
             return pathString.toString();
@@ -112,7 +153,18 @@ public class CampusWalk {
         }
     }
 
+    /**
+     * make the map to be closed
+     */
     public void exit() {
-        map.exit();
+        this.map.exit();
+    }
+
+    public static void main(String[] args) {
+        Hexagon.TIME_DELAY = 500; // Change speed of animation.
+        String file = "map1.txt"; // Change when trying other maps.
+        CampusWalk walk = new CampusWalk(file, true);
+        String result = walk.findPath();
+        System.out.println(result);
     }
 }
